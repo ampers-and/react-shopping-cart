a7850e22603db18b2969c74f2bc31f48fe211631
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-09-09 14:21:30 -0700

    steps 4-6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+
 import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -17,23 +19,24 @@ function App() {
 	};
 
 	return (
-		<ProductContext.Provider value={{ products, addItem }}>
+		<CartContext.Provider value={{cart}}>
+
+			<ProductContext.Provider value={{ products, addItem }}>
 
 			<div className="App">
-			<Navigation cart={cart} />
+				<Navigation />
 
-			{/* Routes */}
-			<Route
-				exact path="/" component={Products}/>
+				{/* Routes */}
+				<Route
+					exact path="/" component={Products}/>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-		
+				<Route
+					path="/cart" component={ShoppingCart}/>
+			</div>
+
 		</ProductContext.Provider>
-	
+
+		</CartContext.Provider>
 	);
 }
 
